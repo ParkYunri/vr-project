2d12aafef80c685b610c237ab01c3fd77b37a676
ParkYunri <yoyoyo066@naver.com>
2022-04-25 23:55:01 +0900

    upload 1

---
+++ 360_video/Assets/Scripts/ChangeVideo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ChangeVideo : MonoBehaviour
+{
+    public VideoClip clip01;
+    public VideoClip clip02;
+    bool isClip02;
+    private int isPlaying = 0;
+
+    public void Change02()
+    {
+        if (!isClip02)
+        {
+            gameObject.GetComponent<VideoPlayer>().clip = clip02;
+            isClip02 = true;
+        }
+        else
+        {
+            if(isPlaying == 0)
+            {
+                gameObject.GetComponent<VideoPlayer>().Pause();
+                isPlaying = 1;
+            }
+            else if(isPlaying == 1)
+            {
+                gameObject.GetComponent<VideoPlayer>().Play();
+                isPlaying = 0;
+            }
+        }
+    }
+
+    public void Change01()
+    {
+        if (isClip02)
+        {
+            gameObject.GetComponent<VideoPlayer>().clip = clip01;
+            isClip02 = false;
+        }
+        else
+        {
+            if(isPlaying == 0)
+            {
+                gameObject.GetComponent<VideoPlayer>().Play();
+                isPlaying = 1;
+            }
+            else if(isPlaying == 1)
+            {
+                gameObject.GetComponent<VideoPlayer>().Pause();
+                isPlaying = 0;
+            }
+        }
+    }
+}
---
+++ 360_video/Assets/Scripts/RayClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayClick : MonoBehaviour
+{
+    private float GaugeTimer = 0.0f;
+    private bool isPlaying = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1000;
+
+        if (Physics.Raycast(transform.position, forward, out hit))
+        {
+            GaugeTimer += 1.0f / 2.0f * Time.deltaTime;
+
+            if (GaugeTimer >= 1.0f)
+            {
+                if (isPlaying) //영상 재생중일때
+                {
+                    hit.transform.GetComponent<Button>().onClick.Invoke();
+                    isPlaying = false;
+                }
+                else if(!isPlaying) //영상 일시정지일때
+                {
+                    hit.transform.GetComponent<Button>().onClick.Invoke();
+                    isPlaying = true;
+                }
+                GaugeTimer = 0.0f;
+            }
+        }
+    }
+}
